982821ade227e3c0755e31b308f8b19b928f26f6
dohyun93 <kwondohyun93@naver.com>
2023-02-21 21:58:01 +0900

    리시버와 메서드

--- chapter19_method/main.go
+++ chapter19_method/main.go
@@ -4,8 +4,9 @@ import "fmt"
 
 func main() {
 	// 메서드를 선언하려면 '리시버'를 func 키워드와 함수 이름 사이에 중괄호로 명시해야 한다.
+	// 리시버가 없으면, 함수다.
 	a := &account{balance: 100}
-	a.withDrathMethod(30)
+	a.withDrawMethod(30)
 	fmt.Println(a.balance)
 }
 
@@ -22,7 +23,9 @@ type account struct {
 	balance int
 }
 
-func (a *account) withDrathMethod(amount int) {
+func (a *account) withDrawMethod(amount int) {
 	// 함수가 아닌 메서드로 표현해보자.
 	a.balance -= amount
+	// 1. withDrawMethod 메서드는 *account 타입에 속한다.
+	// 2. *account 는 리시버 타입이다.
 }
