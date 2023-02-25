3786ed05f3ae1e859dcdf7a052d94dd726ebcfa9
dohyun93 <kwondohyun93@naver.com>
2023-02-25 20:45:59 +0900

    chapter20_interface

--- chapter20_interface/main.go
+++ chapter20_interface/main.go
@@ -3,13 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println()
 	// 인터페이스는 '상호작용'할 수 있는 접점이다.
 	// 인터페이스를 이용하면 추상화된 객체로 상호작용할 수 있다.
 
+	// 2. 인터페이스 사용 예시
+	// 2-1. Student 구조체를 만들어 인스턴스를 생성했다. String() 은 매개변수가 없고 string 반환을 하는 Student 타입에 속하는 메서드이다.
+	student := &Student{"도현", 31}
+	// 2-2. Stringer 인터페이스 변수를 만들었다. 이 인터페이스는 String() 이라는 매개변수가 없고 string 반환을 하는 메서드를 갖는다.
+	var stringer Stringer
+	// 2-3. String() string 메서드를 포함하는 인터페이스에 String() string 메서드를 포함하고 있는 인스턴스를 대입할 수 있다.
+	stringer = student
+
+	fmt.Printf("%s\n", stringer.String())
+
+	// [핵심] - 인터페이스의 메서드를 포함하는 타입은 인터페이스에 할당될 수 있다.
 }
 
-// 인터페이스 선언은 아래처럼 할 수 있다.
+/////////////// 1. 인터페이스 선언은 아래처럼 할 수 있다.
 
 // 인터페이스도 구조체처럼 '타입'중 하나이기 때문에, type을 써줘야 한다.
 // 즉, 인터페이스도 변수 선언이 가능하고, 변수의 값으로 사용할 수 있다.
@@ -25,6 +35,16 @@ type DuckInterface interface {
 	// _(x int) -> 메서드는 반드시 이름이 있어야 한다.
 }
 
+//////////// 2. 인터페이스 사용 예시
 type Stringer interface {
 	String() string
+}
+
+type Student struct {
+	Name string
+	Age  int
+}
+
+func (s Student) String() string {
+	return fmt.Sprintf("안녕, 나는 %d 살 %s 라고 해 \n", s.Age, s.Name)
 }
