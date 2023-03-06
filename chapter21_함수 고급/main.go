af9d01c8de91f278cc362b70b06c4f650e7e9009
dohyun93 <kwondohyun93@naver.com>
2023-03-06 22:34:12 +0900

    chapter21 ... 함수 고급편

---
+++ chapter21_함수 고급/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Sum(nums ...int) int {
+	sum := 0
+	fmt.Printf("nums 타입 : %T\n", nums)
+
+	for _, v := range nums {
+		sum += v
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(Sum(1, 2, 3, 4, 5))
+	fmt.Println(Sum(100, 200))
+}
