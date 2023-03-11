40169c24be48bb6223432dea7b13b9bbe6230bb6
dohyun93 <kwondohyun93@naver.com>
2023-03-11 22:18:09 +0900

    chapter21 ... 함수 고급편

--- chapter21_함수 고급/main.go
+++ chapter21_함수 고급/main.go
@@ -16,8 +16,29 @@ func Sum(nums ...int) int {
 	return sum
 }
 
+// 여러 타입의 인수들을 받아서 출력하는 함수를 만들어보자.
+func Print(args ...interface{}) {
+	for _, val := range args {
+		switch T := val.(type) {
+		case bool:
+			value := val.(bool)
+			fmt.Println("boolean data print!: ", value)
+		case int64:
+			value := val.(int64)
+			fmt.Println("int64 data print!: ", value)
+		default:
+			fmt.Printf("type %T is not supported.\n", T)
+			// fmt.Printf 로 %T 를 이용해 타입출력도 가능하지만, reflect.TypeOf(val) 도 된다.
+		}
+	}
+}
+
 func main() {
 	fmt.Println(Sum(1, 2, 3, 4, 5))
 	fmt.Println(Sum(100, 200))
 	fmt.Println(Sum(10, 20, 40))
+
+	var myInt64 int64 = 10
+	fmt.Println()
+	Print(true, 34, myInt64)
 }
